fe8d3d622af1cb6c2c06be9927a86de146ff9556
LinkFalcon0921 <20198918@itla.edu.do>
2023-05-03 23:00:40 -0400

    refactor: appending userId to credentialsMapper.

--- Domain.Dtos/Mappers/CredentialsMapper.cs
+++ Domain.Dtos/Mappers/CredentialsMapper.cs
@@ -24,6 +24,7 @@ public class CredentialsMapper : IEntityDependantMapper<Credentials, Credentials
         {
             credentials.User = UserMapper.GetInstance()
                 .MapToWithOut(e.User);
+            credentials.UserId = credentials.User.Id;
         }
 
         return credentials;
